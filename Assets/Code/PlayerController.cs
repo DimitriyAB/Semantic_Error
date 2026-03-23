9863fa8ceb2ed40eb2147f7747eacef3a796ffb7
DimitriyAB <dimkagithub3453@mail.ru>
2026-03-23 16:14:34 +0500

    Animation and movement main character
    передвижение игрового персонажа с анимациями по локации

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    public float speed;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+        animator.SetFloat("Speed", direction.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+    }
+}
